fbda040e9d1c4f3cf26b6427b7add0ba9563f851
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-07-21 14:44:32 +0200

    Wzorzec FluentApi przed

---
+++ ZF.DesignPatterns.FluentApiDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZF.DesignPatterns.FluentApiDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var order = Order.Create();
+
+            order.Calculate();
+
+            order.Send();
+
+          //  Order.Create().Calculate().Send();
+        }
+    }
+
+    class Order
+    {
+        public decimal Amount { get; set; }
+
+        public static Order Create()
+        {
+            return new Order();
+        }
+
+        public void Calculate()
+        {
+            this.Amount = 199;
+        }
+
+        public void Send()
+        {
+            Console.WriteLine($"Send order {this.Amount}");
+        }
+
+    }
+}
