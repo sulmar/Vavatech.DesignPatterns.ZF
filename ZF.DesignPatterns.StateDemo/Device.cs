cd40d19d5ae4e069d3e497bcb57d0d86bb9370be
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-07-22 13:42:41 +0200

    Dodanie wzorca State

---
+++ ZF.DesignPatterns.StateDemo/Device.cs
@@ -0,0 +1,80 @@
+﻿using Stateless;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZF.DesignPatterns.StateDemo
+{
+    class Device
+    {
+        private readonly StateMachine<State, Trigger> machine;
+
+        private System.Timers.Timer timer;
+
+        private const int timeout = 5; // in seconds
+
+        public Device()
+        {
+            machine = new StateMachine<State, Trigger>(State.Off);
+
+            machine.Configure(State.Off)
+                .Permit(Trigger.Start, State.On)
+                .OnEntry(Started);
+
+            machine.Configure(State.On)
+                .Permit(Trigger.Stop, State.Off)
+                .Permit(Trigger.ElapsedTime, State.Off)
+                .OnEntry(() => timer.Start());
+
+            // http://www.webgraphviz.com/
+            Console.WriteLine(machine.ToDotGraph());
+
+
+            timer = new System.Timers.Timer(TimeSpan.FromSeconds(timeout).TotalMilliseconds);
+            timer.AutoReset = false;
+            timer.Elapsed += (sender, args) => machine.Fire(Trigger.ElapsedTime);
+        }
+
+        private void Started()
+        {
+            Console.WriteLine("Wyłączone!");
+        }
+
+        public void Start()
+        {
+            machine.Fire(Trigger.Start);
+        }
+
+        public void Stop()
+        {
+            machine.Fire(Trigger.Stop);
+        }
+
+        public State State
+        {
+            get
+            {
+                return machine.State;
+            }
+        }
+    }
+
+    public enum State
+    {
+        On,
+
+        Off
+    }
+
+    public enum Trigger
+    {
+        Start,
+
+        Stop,
+
+        ElapsedTime
+      
+    }
+}
---
+++ ZF.DesignPatterns.StateDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ZF.DesignPatterns.StateDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var device = new Device();
+
+            Console.WriteLine(device.State);
+
+            device.Start();
+
+            Console.WriteLine(device.State);
+
+           // device.Stop();
+
+            Console.WriteLine(device.State);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
